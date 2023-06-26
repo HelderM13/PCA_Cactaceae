b09affc58f83c341d4eab2bf782aafc982168a8b
Hélder Marques <136626662+HelderM13@users.noreply.github.com>
2023-06-26 19:30:56 +0100

    Update Count_SNPS.py

--- Count_SNPS.py
+++ Count_SNPS.py
@@ -2,7 +2,7 @@ def contar_snps(arquivo_vcf):
     contador_snps = 0
     with open(arquivo_vcf, 'r') as arquivo:
         for linha in arquivo:
-            if not linha.startswith('#'):  # Ignorar linhas de cabeçalho
+            if not linha.startswith('#'): 
                 contador_snps += 1
     return contador_snps
 

49b4c98d93fb979e0790653eeeae2251f0dba548
Hélder Marques <136626662+HelderM13@users.noreply.github.com>
2023-06-26 19:35:32 +0100

    Update Count_SNPS.py

--- Count_SNPS.py
+++ Count_SNPS.py
@@ -8,4 +8,4 @@ def contar_snps(arquivo_vcf):
 
 file_name_vcf = '/home/your/path/file.vcf'
 num_snps = contar_snps(file_name_vcf)
-print(f'O arquivo {file_name_vcf} contém {num_snps} SNPs.')
+print(f'The file {file_name_vcf} have {num_snps} SNPs.')
